c8ca4444c546bf7094c4df4407e6fdbdd185099f
Greta Yorsh <gyorsh@janestreet.com>
2019-11-01 13:16:02 +0000

    cleanip

--- src/addr.ml
+++ src/addr.ml
@@ -1,7 +1,2 @@
 open Core
 include Int64
-
-(* (* it should be private but then deriving sexp and hash don't work and need
- *    to be reimplemented manually, and every place we use it needs a
- *    conversion, not only at creation time. hmm.. *)
- * type t = Int64.t [@@deriving compare, sexp, hash, equal] *)
