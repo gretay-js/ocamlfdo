5575fbf8b74588d29bf78a416250c7a35b272961
Greta Yorsh <gyorsh@janestreet.com>
2020-04-10 17:34:23 +0100

    fix check of percent functions

--- src/trim.ml
+++ src/trim.ml
@@ -15,45 +15,50 @@ type t = action list [@@deriving sexp]
 let of_sexp s =
   let t = t_of_sexp s in
   List.iter t ~f:(function
-    | Top_percent p | Top_percent_samples p ->
+      | Top_percent p
+      | Top_percent_samples p ->
         let p = Percent.to_percentage p in
-        if Float.O.(p < 0.0 || p > 1.0) then
+        if Float.O.(p < 0. || p > 100.)
+        then
           Report.user_error
-            "Cannot take %fl percent of functions. Argument must be between \
-             0%% and 100%%."
+            "Cannot take %.0f%% of functions. Argument must be between 0%% and 100%%."
             p
-    | Top _ | Top_clusters _ | Min_samples _ -> ());
+      | Top _ | Top_clusters _ | Min_samples _ -> ());
   t
+;;
 
 let take_top_percent_samples l p =
-  let total =
-    List.fold l ~init:0L ~f:(fun acc (_, count) -> Execount.(acc + count))
-  in
+  let total = List.fold l ~init:0L ~f:(fun acc (_, count) -> Execount.(acc + count)) in
   let max = Percent.apply p (Float.of_int64 total) |> Float.to_int64 in
   let list, sum =
     List.fold l ~init:([], 0L) ~f:(fun (list, sum) (name, count) ->
-        if Execount.compare sum max < 0 then
-          ((name, count) :: list, Execount.(sum + count))
-        else (list, sum))
+        if Execount.compare sum max < 0
+        then (name, count) :: list, Execount.(sum + count)
+        else list, sum)
   in
-  if !verbose then
+  if !verbose
+  then
     printf
-      "take_top_percent_samples %s of total %Ld = %Ld, found %Ld in top %d \
-       functions\n"
-      (Percent.to_string p) total max sum (List.length list);
+      "take_top_percent_samples %s of total %Ld = %Ld, found %Ld in top %d functions\n"
+      (Percent.to_string p)
+      total
+      max
+      sum
+      (List.length list);
   List.rev list
+;;
 
 let apply_action l a =
   match a with
   | Top n -> List.take l n
   | Top_percent p ->
-      let total = List.length l in
-      let n = Percent.apply p (Float.of_int total) |> Float.to_int in
-      List.take l n
+    let total = List.length l in
+    let n = Percent.apply p (Float.of_int total) |> Float.to_int in
+    List.take l n
   | Top_percent_samples p -> take_top_percent_samples l p
   | Top_clusters _ -> failwith "Not implemented"
   | Min_samples n ->
-      List.take_while l ~f:(fun (_, count) ->
-          Execount.(compare count (of_int n)) >= 0)
+    List.take_while l ~f:(fun (_, count) -> Execount.(compare count (of_int n)) >= 0)
+;;
 
 let apply t l = List.fold t ~init:l ~f:apply_action
